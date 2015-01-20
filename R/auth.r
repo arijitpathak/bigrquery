84ef79812eca59b47ff3a656c556119d52a3b1d8
arijitpathak <arijitpathak@users.noreply.github.com>
2015-01-20 16:14:32 +0530

    Update auth.r

--- R/auth.r
+++ R/auth.r
@@ -2,8 +2,10 @@
 google <- oauth_endpoint(NULL, "auth", "token",
  base_url = "https://accounts.google.com/o/oauth2")
 bigqr <- oauth_app("google",
-"465736758727.apps.googleusercontent.com",
-  "fJbIIyoIag0oA6p114lwsV2r")
+#"465736758727.apps.googleusercontent.com",
+"334303937053.apps.googleusercontent.com",
+  "T7UrAaXhl9OmyV2mPcBB1lI3")
+#  "fJbIIyoIag0oA6p114lwsV2r")
 
 
 bq_env <- new.env(parent = emptyenv())
